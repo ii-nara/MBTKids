4162ac107b734f0914c1775db90cf0031f36a525
rhdqngkwk90 <ouorhdqngkwk90@gmail.com>
2024-10-17 13:46:32 +0900

    Feat: 패키지 구조 세팅

---
+++ ii-infrastructure/src/test/java/ureca/TestConfiguration.java
@@ -0,0 +1,12 @@
+package ureca; // package com.ureca;
+//
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.autoconfigure.domain.EntityScan;
+// import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+// JPA 의존성 추가하고 주석 풀기
+// @SpringBootApplication
+// @EnableJpaRepositories(basePackages = "com.ureca.repository")
+// @EntityScan(basePackages = "com.ureca.entity")
+// public class TestConfiguration {
+//
+// }
---
+++ ii-infrastructure/src/test/java/ureca/entity/TheaterInfoTest.java
@@ -0,0 +1,29 @@
+package ureca.entity; // package com.ureca.entity;
+//
+// import com.ureca.domain.repository.TheaterInfoRepository;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// 엔티티 테스트 예시 파일입니다~
+// @SpringBootTest
+// class TheaterInfoTest {
+//
+//    @Autowired TheaterInfoRepository theaterInfoRepository;
+//
+//    @Test
+//    void test2() {
+//        // 상영관 테스트 데이터
+//        TheaterInfoEntity theaterInfo =
+//                TheaterInfoEntity.builder()
+//                        .theaterId("107")
+//                        .theaterNm("7관")
+//                        .theaterCd("S2")
+//                        .totalSeats(120)
+//                        .build();
+//
+//        // theaterInfo 저장
+//        theaterInfoRepository.save(theaterInfo);
+//
+//        System.out.println("===================================");
+//    }
+// }

23c7cad66771994d31429c717533e823669d7889
rladbstn1000 <614edison@gmail.com>
2024-10-18 15:37:36 +0900

    Feat: childEntity 개발

---
+++ ii-infrastructure/src/main/java/com/ureca/entity/ChildEntity.java
@@ -0,0 +1,47 @@
+package com.ureca.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Table(name = "child_user")
+public class ChildEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "childId", nullable = false)
+  private Long childId;
+
+  @Column(name = "parentId", nullable = false)
+  private Long parentId;
+
+  @Column(name = "childName", nullable = false)
+  private String childName;
+
+  @Column(name = "childAge", nullable = false)
+  private int childAge;
+
+  @Column(name = "createdAt", nullable = false)
+  private LocalDateTime createdAt;
+
+  @Column(name = "isTypeDeleted", nullable = false)
+  @ColumnDefault("false")
+  private boolean isTypeDeleted;
+}
---
+++ ii-infrastructure/src/main/java/com/ureca/repository/ChildRepository.java
@@ -0,0 +1,8 @@
+package com.ureca.repository;
+
+import com.ureca.entity.ChildEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChildRepository extends JpaRepository<ChildEntity, Long> {
+
+}

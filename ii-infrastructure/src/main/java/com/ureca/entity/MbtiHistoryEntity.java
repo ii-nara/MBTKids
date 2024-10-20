149618f65c05eecf3e72d6b2cf1a23973a18655e
rlawotmd <kimjs2400@naver.com>
2024-10-20 18:16:34 +0900

    Style: import 추가, 이전 브렌치 코드 삭제

--- ii-infrastructure/src/main/java/com/ureca/entity/MbtiHistoryEntity.java
+++ ii-infrastructure/src/main/java/com/ureca/entity/MbtiHistoryEntity.java
@@ -2,14 +2,21 @@ package com.ureca.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
 @Table(name = "mbti_history")
--- ii-infrastructure/src/main/java/com/ureca/repository/FeedBackRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ureca.repository;
-
-import com.ureca.entity.FeedBackEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeedBackRepository extends JpaRepository<FeedBackEntity, Long> {}

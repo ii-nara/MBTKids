b8d938944dc50f9ae824a6d9e070fe87738f5840
rlawotmd <kimjs2400@naver.com>
2024-10-21 10:11:36 +0900

    Feat: 피드백 엔티티, 레포 추가

---
+++ ii-infrastructure/src/main/java/com/ureca/repository/FeedbackStatusRepository.java
@@ -0,0 +1,8 @@
+package com.ureca.repository;
+
+import com.ureca.entity.FeedbackStatusEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedbackStatusRepository extends JpaRepository<FeedbackStatusEntity, Long> {
+
+}

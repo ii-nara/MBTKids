6e7fefe51ee75acb0946d0181d128a45f225a0e9
delight-es <skfmf7878@gmail.com>
2024-10-22 15:05:10 +0900

    Feat: MBTI 질문 관련 도메인 객체 추가

---
+++ ii-domain/src/main/java/com/ureca/domain/MbtiQuestion.java
@@ -0,0 +1,23 @@
+package com.ureca.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/* 개별 MBTI 질문 구조 */
+@Getter
+public class MbtiQuestion {
+
+  // 순서
+  private final int order;
+  // 질문 내용
+  private final String content;
+  // 질문별 성향 (약한/강한)
+  private final String type;
+
+  @Builder
+  public MbtiQuestion(int order, String content, String type) {
+    this.order = order;
+    this.content = content;
+    this.type = type;
+  }
+}

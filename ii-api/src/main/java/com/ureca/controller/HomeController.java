aa210b4ac2446bd988de5ab5f9679fcf1a7ee4d0
hoya <sv05159@naver.com>
2024-10-23 10:18:26 +0900

    Feat: 도서 페이징 title 추가

--- ii-api/src/main/java/com/ureca/controller/HomeController.java
+++ ii-api/src/main/java/com/ureca/controller/HomeController.java
@@ -50,16 +50,21 @@ public class HomeController {
     Long childId = 1L;  //TODO session
 
     List<BookInfo> books = new ArrayList<>();
+    String title = "도서 목록";
     if (Objects.equals("SIMILAR", type)) {
+      title = "아이 성향 추천";
       books = recommendService.recommendSimilarBooks(childId, page * size, size);
     }
     if (Objects.equals("OPPOSITE", type)) {
+      title = "반대 성향 추천";
       books = recommendService.recommendOppositeBooks(childId, page * size, size);
     }
     if (Objects.equals("LIKE", type)) {
+      title = "유사 성향 아이들 추천";
       books = recommendService.recommendSimilarChildLikedBooks(childId, page * size, size);
     }
     model.addAttribute("books", books);
+    model.addAttribute("title", title);
 
     // 현재 페이지 번호와 다음 페이지가 있는지 여부를 추가
     model.addAttribute("type", type);

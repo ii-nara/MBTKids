84d362be9a535ed32911e1e7008e7a73b49f78f7
rlawotmd <kimjs2400@naver.com>
2024-10-24 11:34:52 +0900

    Remove: 피드백 엔티티 일부 삭제

--- ii-infrastructure/src/main/java/com/ureca/repository/FeedbackLogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ureca.repository;
-
-import com.ureca.entity.FeedbackLogEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeedbackLogRepository extends JpaRepository<FeedbackLogEntity, Long> {
-
-}

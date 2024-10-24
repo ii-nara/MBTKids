af109a125b44b699e6918a1742a994e7041475fe
hoya <sv05159@naver.com>
2024-10-24 15:43:06 +0900

    Feat: 추천 타입 아닌 경우 홈화면 리다이렉트

--- ii-api/src/main/java/com/ureca/controller/HomeController.java
+++ ii-api/src/main/java/com/ureca/controller/HomeController.java
@@ -1,9 +1,9 @@
 package com.ureca.controller;
 
+import com.ureca.constant.RecommendationType;
 import com.ureca.dto.BookInfo;
 import com.ureca.dto.BookPage;
 import com.ureca.service.RecommendService;
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -50,24 +50,16 @@ public class HomeController {
       @RequestParam(value = "size", defaultValue = "" + DEFAULT_LIMIT) int size) {
     Long childId = 1L;  //TODO session
 
-    BookPage<BookInfo> books = null;
-    String title = "도서 목록";
-    if (Objects.equals("SIMILAR", type)) {
-      title = "아이 성향 추천";
-      books = recommendService.recommendSimilarBooks(childId, page * size, size);
-    }
-    if (Objects.equals("OPPOSITE", type)) {
-      title = "반대 성향 추천";
-      books = recommendService.recommendOppositeBooks(childId, page * size, size);
-    }
-    if (Objects.equals("LIKE", type)) {
-      title = "유사 성향 아이들 추천";
-      books = recommendService.recommendSimilarChildLikedBooks(
-          childId, page * size, size);
+    RecommendationType recommendationType;
+    try {
+      recommendationType = RecommendationType.valueOf(type.toUpperCase());
+    } catch (IllegalArgumentException e) {
+      return "redirect:/mbtkids/home";
     }
 
-    model.addAttribute("books", books);
-    model.addAttribute("title", title);
+    model.addAttribute("books",
+        recommendationType.recommend(recommendService, childId, page, size));
+    model.addAttribute("title", recommendationType.getTitle());
     model.addAttribute("type", type);
     model.addAttribute("currentPage", page);
     model.addAttribute("pageSize", size);

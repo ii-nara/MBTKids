cfef8ec71f365e74fbf127fad8a296dc686a668f
delight-es <skfmf7878@gmail.com>
2024-10-25 15:55:08 +0900

    Feat: 자녀 세션 연결, 결과 저장 기능 추가

--- ii-api/src/main/java/com/ureca/controller/MbtiTestController.java
+++ ii-api/src/main/java/com/ureca/controller/MbtiTestController.java
@@ -1,15 +1,21 @@
 package com.ureca.controller;
 
+import com.ureca.config.auth.PrincipalDetails;
+import com.ureca.entity.ChildEntity;
 import com.ureca.model.MbtiQuestion;
 import com.ureca.model.MbtiQuestionProvider;
 import com.ureca.dto.MbtiInfoResponseDto;
 import com.ureca.dto.MbtiStatusResponseDto;
+import com.ureca.service.ChildAddServiceImpl;
 import com.ureca.service.MbtiInfoService;
+import com.ureca.service.MbtiManagementService;
 import com.ureca.service.MbtiTestService;
 import java.util.List;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,16 +26,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MbtiTestController {
 
   private final MbtiQuestionProvider mbtiQuestionProvider;
-  private final MbtiTestService mbtiService;
+  private final MbtiTestService mbtiTestService;
   private final MbtiInfoService mbtiInfoService;
+  private final MbtiManagementService mbtiManagementService;
 
   public MbtiTestController(
-      MbtiTestService mbtiService,
+      MbtiTestService mbtiTestService,
       MbtiQuestionProvider mbtiQuestionProvider,
-      MbtiInfoService mbtiInfoService) {
-    this.mbtiService = mbtiService;
+      MbtiInfoService mbtiInfoService,
+      MbtiManagementService mbtiManagementService) {
+    this.mbtiTestService = mbtiTestService;
     this.mbtiQuestionProvider = mbtiQuestionProvider;
     this.mbtiInfoService = mbtiInfoService;
+    this.mbtiManagementService = mbtiManagementService;
   }
 
   // 1. 질문 조회
@@ -41,30 +50,40 @@ public class MbtiTestController {
     return "mbti/test";
   }
 
-  // 2. 응답 처리
-  @PostMapping("/result")
-  public String postMbtiResult(@RequestParam("answers") String answers, Model model) {
-    // TODO : 자녀 아이디 연결
-    Long childId = 1L;
-    // 답변
-    List<Integer> answerList = mbtiService.jsonStrToList(answers);
-    if (answerList.isEmpty()) {
-      model.addAttribute("error", "[404] 다시 검사를 시도해주세요.");
-      // TODO : 에러 페이지 구현
-      return "mbti/error";
-    }
-    // MBTI 성향, 강도 계산
-    MbtiStatusResponseDto mbtiTestRequestDto = mbtiService.processMbtiAnswer(childId, answerList);
-    String mbtiType = mbtiTestRequestDto.getMbtiType();
+  // 2. 결과 저장
+  @PostMapping("/save")
+  public String postMbtiResult(@AuthenticationPrincipal PrincipalDetails principalDetails,
+      @RequestParam("answers") String answers) {
+    Long childId = principalDetails.getChild().getChildId();
+    // 답변 처리
+    List<Integer> answerList = mbtiTestService.jsonStrToList(answers);
+    // MBTI 성향, 강도 계산 및 저장
+    mbtiTestService.processMbtiAnswer(childId, answerList);
+    return "redirect:/mbtkids/mbti/result";
+  }
+
+  // 3. 결과 조회
+  @GetMapping("/result")
+  public String getMbtiResult(@AuthenticationPrincipal PrincipalDetails principalDetails,
+      Model model) {
+    // 성향 조회
+    Long childId = principalDetails.getChild().getChildId();
+    MbtiStatusResponseDto mbtiTestResDto = mbtiManagementService.getMbtiStatus(childId);
+    String mbtiType = mbtiTestResDto.getMbtiType();
+    int scoreI = mbtiTestResDto.getTypeI(), scoreE = mbtiTestResDto.getTypeE(),
+        scoreS = mbtiTestResDto.getTypeS(), scoreN = mbtiTestResDto.getTypeN(),
+        scoreT = mbtiTestResDto.getTypeT(), scoreF = mbtiTestResDto.getTypeF(),
+        scoreP = mbtiTestResDto.getTypeP(), scoreJ = mbtiTestResDto.getTypeJ();
+
     model.addAttribute("mbtiType", mbtiType);
-    model.addAttribute("scoreI", mbtiTestRequestDto.getTypeI());
-    model.addAttribute("scoreE", mbtiTestRequestDto.getTypeE());
-    model.addAttribute("scoreS", mbtiTestRequestDto.getTypeS());
-    model.addAttribute("scoreN", mbtiTestRequestDto.getTypeN());
-    model.addAttribute("scoreT", mbtiTestRequestDto.getTypeT());
-    model.addAttribute("scoreF", mbtiTestRequestDto.getTypeF());
-    model.addAttribute("scoreP", mbtiTestRequestDto.getTypeP());
-    model.addAttribute("scoreJ", mbtiTestRequestDto.getTypeJ());
+    model.addAttribute("scoreI", mbtiTestService.changeRatio(scoreI, scoreE));
+    model.addAttribute("scoreE", mbtiTestService.changeRatio(scoreE, scoreI));
+    model.addAttribute("scoreS", mbtiTestService.changeRatio(scoreS, scoreN));
+    model.addAttribute("scoreN", mbtiTestService.changeRatio(scoreN, scoreS));
+    model.addAttribute("scoreT", mbtiTestService.changeRatio(scoreT, scoreF));
+    model.addAttribute("scoreF", mbtiTestService.changeRatio(scoreF, scoreT));
+    model.addAttribute("scoreP", mbtiTestService.changeRatio(scoreP, scoreJ));
+    model.addAttribute("scoreJ", mbtiTestService.changeRatio(scoreJ, scoreP));
     MbtiInfoResponseDto mbtiInfoResponseDto = mbtiInfoService.getMbtiNmInfo(mbtiType);
     model.addAttribute("mbtiInfo", mbtiInfoResponseDto);
     return "mbti/result";

9a7398f7fb2060e2f86a6a96bb363a7591b11d3c
rladbstn1000 <83768801+rladbstn1000@users.noreply.github.com>
2024-10-29 01:23:17 +0900

    Update CustomBCryptPasswordEncoder.java

--- ii-api/src/main/java/com/ureca/config/CustomBCryptPasswordEncoder.java
+++ ii-api/src/main/java/com/ureca/config/CustomBCryptPasswordEncoder.java
@@ -4,6 +4,4 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CustomBCryptPasswordEncoder extends BCryptPasswordEncoder {
-  
-}
+public class CustomBCryptPasswordEncoder extends BCryptPasswordEncoder {}

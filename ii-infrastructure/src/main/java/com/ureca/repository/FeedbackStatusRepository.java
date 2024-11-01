f3cb286fc011597f4de3b8d339de75984be98ce9
rhdqngkwk90 <ouorhdqngkwk90@gmail.com>
2024-11-01 13:57:51 +0900

    Fix: 린트 적용

--- ii-infrastructure/src/main/java/com/ureca/repository/FeedbackStatusRepository.java
+++ ii-infrastructure/src/main/java/com/ureca/repository/FeedbackStatusRepository.java
@@ -3,9 +3,6 @@ package com.ureca.repository;
 import com.ureca.entity.FeedbackStatusEntity;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface FeedbackStatusRepository extends JpaRepository<FeedbackStatusEntity, Long> {
   Optional<FeedbackStatusEntity> findByBookEntity_BookIdAndChildEntity_ChildId(

09184a4a686d95f86a35e52b95703592919e4ef0
김재승 <117148033+rlawotmd@users.noreply.github.com>
2024-11-03 02:57:52 +0900

    Style: Builder 어노테이션 제거

--- ii-domain/src/main/java/com/ureca/dto/EventSaveRequestDto.java
+++ ii-domain/src/main/java/com/ureca/dto/EventSaveRequestDto.java
@@ -1,12 +1,10 @@
 package com.ureca.dto;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Builder
 public class EventSaveRequestDto {
 
   private String name;

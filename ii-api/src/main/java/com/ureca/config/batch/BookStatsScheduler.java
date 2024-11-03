973313a398f4246b6d78a1659a2ee7a8e377de78
rhdqngkwk90 <ouorhdqngkwk90@gmail.com>
2024-11-03 18:39:03 +0900

    Feat: 관리자웹 도서 통계 기능 추가(스케줄러+엑셀다운로드)

---
+++ ii-api/src/main/java/com/ureca/config/batch/BookStatsScheduler.java
@@ -0,0 +1,25 @@
+package com.ureca.config.batch;
+
+import com.ureca.service.BookService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookStatsScheduler {
+
+  @Autowired
+  private BookService bookService;
+
+  /**
+   * 매일 새벽 2시에 도서 통계 등록
+   */
+  @Scheduled(cron = "0 0 2 * * ?")
+  public void scheduleBookStatistics() {
+    // 모든 도서 ID를 조회
+    List<Long> bookIdList = bookService.getBookIdList();
+    // 각 도서 ID에 대해 통계 정보 저장
+    for (Long bookId : bookIdList) bookService.saveBookStatistics(bookId);
+  }
+}
